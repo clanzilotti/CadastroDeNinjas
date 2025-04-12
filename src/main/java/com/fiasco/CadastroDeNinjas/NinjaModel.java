eabdd1568b008c9d3e67af882e1fb88b076f6876
Claudio Lanzilotti <clanzilotti@gmail.com>
2025-04-12 08:48:24 -0300

    Classe Ninja.

---
+++ src/main/java/com/fiasco/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,24 @@
+package com.fiasco.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tCadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+}
